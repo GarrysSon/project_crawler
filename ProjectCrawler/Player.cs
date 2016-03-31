19cdf92adeba83665019d5e16265dace9f4a12b9
unknown <john.garrison@ou.edu>
2016-03-30 19:57:43 -0500

    Created the Player class and implimented most of its needed components.

---
+++ ProjectCrawler/Player.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.ComponentModel;
+
+namespace ProjectCrawler
+{
+    /// <summary>
+    /// Class representing a player object.
+    /// </summary>
+    public class Player : GameObject
+    {
+        private const int MAX_HEALTH = 100;
+
+        /// <summary>
+        /// The Player's health.
+        /// </summary>
+        [DefaultValue(MAX_HEALTH)]
+        public int Health { get; set; }
+
+        /// <summary>
+        /// The Player's level.
+        /// </summary>
+        [DefaultValue(0)]
+        public int Level { get; set; }
+
+        /// <summary>
+        /// The Player's current position.
+        /// </summary>
+        private Vector2 position;
+
+        /// <summary>
+        /// The Player's bounding box.
+        /// </summary>
+        private Rectangle boundingBox;
+
+        /// <summary>
+        /// The default constructor for the Player object.
+        /// </summary>
+        public Player()
+        {
+            this.position = new Vector2();
+            this.boundingBox = new Rectangle();
+        }
+
+        /// <summary>
+        /// The getter for the position of the Player.
+        /// </summary>
+        public Vector2 Position
+        {
+            get { return this.position; }
+        }
+
+        /// <summary>
+        /// The getter for the bounding box of the Player.
+        /// </summary>
+        public Rectangle BoundingBox
+        {
+            get { return this.boundingBox; }
+        }
+
+        /// <summary>
+        /// The update method for the Player.
+        /// </summary>
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The render method for the Player.
+        /// </summary>
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
