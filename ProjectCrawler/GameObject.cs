41d524a83c79b4f4777c8532ebdaf4ddc4f71585
Chris Hackmann <Chris Hackmann>
2016-03-30 19:28:26 -0500

    Added GameObject interface.

---
+++ ProjectCrawler/GameObject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectCrawler
+{
+    public interface GameObject
+    {
+        Vector2 Position
+        {
+            get;
+        }
+
+        Rectangle BoundingBox
+        {
+            get;
+        }
+
+        void Update();
+
+        void Render();
+    }
+}
