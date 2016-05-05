7a53b00166b7a83338b5e67b108f88c6e91000b5
Chris Hackmann <cphackm@gmail.com>
2016-05-05 00:06:25 -0500

    Can now spawn the two different enemy types.

--- ProjectCrawler/Objects/Game/Level/MainArea.cs
+++ ProjectCrawler/Objects/Game/Level/MainArea.cs
@@ -6,6 +6,8 @@ using ProjectCrawler.Objects.Generic.Utility;
 using ProjectCrawler.Objects.Game.Player;
 using ProjectCrawler.Objects.Game.Enemy;
 using ProjectCrawler.Objects.Game.Level.Component;
+using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace ProjectCrawler.Objects.Game.Level
 {
@@ -14,6 +16,9 @@ namespace ProjectCrawler.Objects.Game.Level
     /// </summary>
     public class MainArea : GameLevel
     {
+        // Stuff for enemy spawning.
+        bool isSpawnKeyPressed;
+
         /// <summary>
         /// Base constructor.
         /// </summary>
@@ -51,6 +56,7 @@ namespace ProjectCrawler.Objects.Game.Level
             PolyWall wallObject = new PolyWall(wallShape, GlobalConstants.TOWER_ROOM_IMAGE_TAG);
             this.RegisterGameObject(wallObject);
             this.StoreValue(GlobalConstants.TEST_WALL_TAG, wallObject);
+            isSpawnKeyPressed = false;
         }
 
         /// <summary>
@@ -63,6 +69,65 @@ namespace ProjectCrawler.Objects.Game.Level
 
             // Finish rendering the level as normal
             base.Render();
+        }
+
+        /// <summary>
+        /// Updates the level.
+        /// </summary>
+        public override void Update()
+        {
+            // Grab the player
+            PlayerNinja player = RetrieveValue<PlayerNinja>(GlobalConstants.PLAYER_TAG);
+            // Create new enemies if corresponding keys are pressed.
+            Random rand = new Random();
+            KeyboardState currentState = Keyboard.GetState();
+            if (currentState.IsKeyDown(Keys.D1))
+            {
+                if (!isSpawnKeyPressed)
+                {
+                    isSpawnKeyPressed = true;
+                    // Create a funny enemy
+                    while (true)
+                    {
+                        double angle = rand.NextDouble() * Math.PI * 2;
+                        Vector2 pos = new Vector2((float)Math.Cos(angle) * 400, (float)Math.Sin(angle) * 400);
+                        if ((pos - player.Position).Length() < 200)
+                        {
+                            continue;
+                        }
+                        FunnyEnemy fe = new FunnyEnemy(
+                            pos,
+                            new Vector2((float)rand.NextDouble() - 0.5f, (float)rand.NextDouble() - 0.5f));
+                        RegisterGameObject(fe);
+                        break;
+                    }
+                }
+            }
+            else if (currentState.IsKeyDown(Keys.D2))
+            {
+                if (!isSpawnKeyPressed)
+                {
+                    isSpawnKeyPressed = true;
+                    // Create a laser enemy
+                    while (true)
+                    {
+                        double angle = rand.NextDouble() * Math.PI * 2;
+                        Vector2 pos = new Vector2((float)Math.Cos(angle) * 400, (float)Math.Sin(angle) * 400);
+                        if ((pos - player.Position).Length() < 200)
+                        {
+                            continue;
+                        }
+                        LazerEnemy le = new LazerEnemy(pos);
+                        RegisterGameObject(le);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                isSpawnKeyPressed = false;
+            }
+            base.Update();
         }
 
         /// <summary>
